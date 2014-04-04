9e4ebbab50b780b8c25a30b863485cbfbf943ceb
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-04-05 03:26:25 +0900

    oops: mv wrong direction, do only succeeded
    - overwrite always

--- do-encode.720p.sh
+++ do-encode.720p.sh
@@ -2,5 +2,8 @@
 
 OUT=$(echo "$1"|sed -r -e 's/(\.(720p|1080p)\.mp4)?.ts$/.720p.mp4/g')
 OUT_PROGRESS="${OUT}.progress"
-/usr/bin/time -p ffmpeg -i "$1" -f mp4 -vcodec libx264 -vpre libx264-hq-ts -aspect 16:9 -s 1280x720 -crf 24 -acodec libfaac -ac 2 -ar 48000 -ab 128k "${OUT_PROGRESS}"
-mv "${OUT}" "${OUT_PROGRESS}"
+/usr/bin/time -p ffmpeg -y -i "$1" -f mp4 -vcodec libx264 -vpre libx264-hq-ts -aspect 16:9 -s 1280x720 -crf 24 -acodec libfaac -ac 2 -ar 48000 -ab 128k "${OUT_PROGRESS}"
+retval=$?
+if [ "_${retval}" = "0" ]; then
+  mv "${OUT_PROGRESS}" "${OUT}"
+fi
