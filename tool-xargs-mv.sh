433fef739847266034925f21f2d913726fd6eeec
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-04-05 03:22:25 +0900

    Directory auto creation

--- tool-xargs-mv.sh
+++ tool-xargs-mv.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-if [ ! -d "$1" ]; then
+if [ -e "$1" -a ! -d "$1" ]; then
   echo "abunai"
   exit 1
 fi
+
+if [ ! -e "$1" ]; then
+  echo "creating directory"
+  mkdir -p $1
+  retval=$?
+  if [ "_$retval" != "_0" ]; then
+    echo "!?"
+    exit $retval
+  fi
+fi
+
 xargs -I __foo__ -- sh -c 'ls -1 __foo__*'|xargs -I __bar__ mv __bar__ "$1"
