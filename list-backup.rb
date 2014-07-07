8f683888c5d36154ebe8c673efcffddc2ed9ea0b
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-07-08 03:16:17 +0900

    show low redundancy later

--- list-backup.rb
+++ list-backup.rb
@@ -5,7 +5,7 @@ abort "usage #{File.basename($0)} dir dir dir ..." if ARGV.empty?
 warnings = []
 notices = []
 
-ARGV.map { |_|
+puts ARGV.map { |_|
   Dir[File.join(File.expand_path(_), '*_*', '*')].select { |__|
     File.directory?(__) && /\d+_\d+\// === __
   }.map { |__| 
@@ -16,7 +16,6 @@ ARGV.map { |_|
 }.sort_by(&:first).map { |basename, paths|
  paths = paths.map(&:first)
 
- puts "#{paths.size}\t#{basename}: #{paths.join(' ')}"
 
  files = Hash[paths.map do |path|
    [path, Dir[File.join(path, basename, '*.mp4')]]
@@ -39,7 +38,9 @@ ARGV.map { |_|
  if (gr.size-bs.size).abs <= 2
    notices << "??? #{basename}: GR=#{gr.size}, BS=#{bs.size}"
  end
-}
+
+ "#{paths.size}\t#{basename}: #{paths.join(' ')}"
+}.sort_by { |_| _.split(/\t/,2).first.to_i * -1 }
 
 puts "---"
 
