2cc51a404e4a6cb2c369ae83f334431f10291df9
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-07-11 20:39:14 +0900

    suitable name

--- list-backup.rb
+++ list-backup.rb
@@ -16,20 +16,20 @@ archives_by_series = ARGV.flat_map { |backup_root|
   dir[parent.size.succ .. -1]
 }
 
-puts archives_by_series.map { |series, roots_and_series_paths|
-  paths = roots_and_series_paths.map(&:first)
+puts archives_by_series.map { |series, archives_and_series_paths|
+  archives = archives_and_series_paths.map(&:first)
 
-  files = Hash[paths.map do |path|
+  files = Hash[archives.map do |path|
     [path, Dir[File.join(path, series, '*.mp4')]]
   end]
 
   names = files.values.flatten.group_by {|_| File.basename(_) }
   names.each do |name, filepaths|
-    if filepaths.size != paths.size
+    if filepaths.size != archives.size
       dirs = filepaths.map { |_| File.dirname(_) }
       @warnings.push <<-EOM
-!!!!\t#{series}/#{name}\t(#{filepaths.size}/#{paths.size})
-#{paths.map { |_| dirs.include?(File.join(_, series)) ? "!!!!\t  + #{_}" : "!!!!\t  - #{_}" }.join("\n")}
+!!!!\t#{series}/#{name}\t(#{filepaths.size}/#{archives.size})
+#{archives.map { |_| dirs.include?(File.join(_, series)) ? "!!!!\t  + #{_}" : "!!!!\t  - #{_}" }.join("\n")}
       EOM
     end
   end
@@ -41,7 +41,7 @@ puts archives_by_series.map { |series, roots_and_series_paths|
     @notices << "??? #{series}: GR=#{gr.size}, BS=#{bs.size}"
   end
 
-  "#{paths.size}\t#{series}: #{paths.join(' ')}"
+  "#{archives.size}\t#{series}: #{archives.join(' ')}"
 }.sort_by { |_| _.split(/\t/,2).first.to_i * -1 }
 
 puts "---"
