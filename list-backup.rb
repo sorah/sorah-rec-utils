f966ee81f9d67f5834fe567139fe75c93d285aa2
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-07-11 20:26:58 +0900

    suitable name

--- list-backup.rb
+++ list-backup.rb
@@ -16,12 +16,12 @@ archives_by_series = ARGV.flat_map { |backup_root|
   dir[parent.size.succ .. -1]
 }
 
-puts archives_by_series.map { |basename, paths|
+puts archives_by_series.map { |series, paths|
   paths = paths.map(&:first)
 
 
   files = Hash[paths.map do |path|
-    [path, Dir[File.join(path, basename, '*.mp4')]]
+    [path, Dir[File.join(path, series, '*.mp4')]]
   end]
 
   names = files.values.flatten.group_by {|_| File.basename(_) }
@@ -29,8 +29,8 @@ puts archives_by_series.map { |basename, paths|
     if filepaths.size != paths.size
       dirs = filepaths.map { |_| File.dirname(_) }
       @warnings.push <<-EOM
-!!!!\t#{basename}/#{name}\t(#{filepaths.size}/#{paths.size})
-#{paths.map { |_| dirs.include?(File.join(_, basename)) ? "!!!!\t  + #{_}" : "!!!!\t  - #{_}" }.join("\n")}
+!!!!\t#{series}/#{name}\t(#{filepaths.size}/#{paths.size})
+#{paths.map { |_| dirs.include?(File.join(_, series)) ? "!!!!\t  + #{_}" : "!!!!\t  - #{_}" }.join("\n")}
       EOM
     end
   end
@@ -39,10 +39,10 @@ puts archives_by_series.map { |basename, paths|
   bs = names.keys.grep(/_BS/)
 
   if (gr.size-bs.size).abs <= 2
-    @notices << "??? #{basename}: GR=#{gr.size}, BS=#{bs.size}"
+    @notices << "??? #{series}: GR=#{gr.size}, BS=#{bs.size}"
   end
 
-  "#{paths.size}\t#{basename}: #{paths.join(' ')}"
+  "#{paths.size}\t#{series}: #{paths.join(' ')}"
 }.sort_by { |_| _.split(/\t/,2).first.to_i * -1 }
 
 puts "---"
