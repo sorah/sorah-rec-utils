16afa57f5b0c0150434501c286d19a0a091a5805
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-07-11 20:15:21 +0900

    flat_map

--- list-backup.rb
+++ list-backup.rb
@@ -5,14 +5,14 @@ abort "usage #{File.basename($0)} dir dir dir ..." if ARGV.empty?
 @warnings = []
 @notices = []
 
-puts ARGV.map { |backup_root|
+puts ARGV.flat_map { |backup_root|
   Dir[File.join(File.expand_path(backup_root), '*_*', '*')].select { |series_dir|
     # make sure that is directory
     File.directory?(series_dir) && /\d+_\d+\// === series_dir
   }.map { |series_dir| 
     [backup_root, series_dir]
   }
-}.flatten(1).group_by { |parent, dir|
+}.group_by { |parent, dir|
   dir.split(File::SEPARATOR)[-2,2].join('/')
 }.sort_by(&:first).map { |basename, paths|
  paths = paths.map(&:first)
