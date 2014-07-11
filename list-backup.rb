66d3e10286f28c644932d120e3863e36fab4951a
Shota Fukumori (sora_h) <sorah@tubusu.net>
2014-07-11 20:14:10 +0900

    Give them a name

--- list-backup.rb
+++ list-backup.rb
@@ -5,11 +5,12 @@ abort "usage #{File.basename($0)} dir dir dir ..." if ARGV.empty?
 @warnings = []
 @notices = []
 
-puts ARGV.map { |_|
-  Dir[File.join(File.expand_path(_), '*_*', '*')].select { |__|
-    File.directory?(__) && /\d+_\d+\// === __
-  }.map { |__| 
-    [_, __]
+puts ARGV.map { |backup_root|
+  Dir[File.join(File.expand_path(backup_root), '*_*', '*')].select { |series_dir|
+    # make sure that is directory
+    File.directory?(series_dir) && /\d+_\d+\// === series_dir
+  }.map { |series_dir| 
+    [backup_root, series_dir]
   }
 }.flatten(1).group_by { |parent, dir|
   dir.split(File::SEPARATOR)[-2,2].join('/')
