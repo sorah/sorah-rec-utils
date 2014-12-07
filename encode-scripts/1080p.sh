87b336160755d091b1e4a062aa3056fb2a9186c3
Shota Fukumori (sora_h) <her@sorah.jp>
2014-12-08 03:19:41 +0900

    -ab 192k

--- encode-scripts/1080p.sh
+++ encode-scripts/1080p.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/time -p ffmpeg -y -i "$1" -deinterlace -f mp4 -vcodec libx264 -vpre libx264-hq-ts -bufsize 20000k -maxrate 25000k -acodec libfaac -ac 2 -ar 48000 -ab 128k -threads 4 "$2"
+/usr/bin/time -p ffmpeg -y -i "$1" -deinterlace -f mp4 -vcodec libx264 -vpre libx264-hq-ts -bufsize 20000k -maxrate 25000k -acodec libfaac -ac 2 -ar 48000 -ab 192k "$2"
--- encode-scripts/720p.sh
+++ encode-scripts/720p.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/time -p ffmpeg -y -i "$1" -f mp4 -vcodec libx264 -vpre libx264-hq-ts -aspect 16:9 -s 1280x720 -crf 24 -acodec libfaac -ac 2 -ar 48000 -ab 128k "$2"
+/usr/bin/time -p ffmpeg -y -i "$1" -f mp4 -vcodec libx264 -vpre libx264-hq-ts -aspect 16:9 -s 1280x720 -crf 24 -acodec libfaac -ac 2 -ar 48000 -ab 192k "$2"
